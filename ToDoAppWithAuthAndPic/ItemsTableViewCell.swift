b8e030e165bd2814eb91ed17900352c6390f5c0b
bilaldrndo <drndob2003@gmail.com>
2018-03-21 19:00:18 +0100

    All outlets are now connected, ItemsTableViewCell class is created.

---
+++ ToDoAppWithAuthAndPic/ItemsTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ItemsTableViewCell.swift
+//  ToDoAppWithAuthAndPic
+//
+//  Created by Bilal on 3/21/18.
+//  Copyright © 2018 Bilal. All rights reserved.
+//
+
+import UIKit
+
+class ItemsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var itemLabel: UILabel!
+    
+}
